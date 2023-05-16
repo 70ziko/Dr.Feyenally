9bdeda635fa768e4e6ecd97922e639c82d217a7e
PatrykF <103563386+70ziko@users.noreply.github.com>
2023-05-16 16:22:37 +0200

    frankenstein

--- app.py
+++ app.py
@@ -1,39 +1,35 @@
-from flask import Flask, render_template, request, jsonify
+#imports
+from flask import Flask, render_template, request
 import openai
 import os
 
-# Load OpenAI API key from environment variable
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Initialize Flask app
 app = Flask(__name__)
+#create chatbot
 
-# Define home route
-@app.route('/')
-def home():
-    return render_template('index.html')
+#define app routes
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# Define chatbot route
-@app.route('/chatbot', methods=['POST'])
-def chatbot():
-    # Get user input from request
-    input_text = request.form['chat-form']
-
-    # Call OpenAI API to generate response
+@app.route("/get")
+#function for the bot response
+def get_bot_response():
+    user_text = request.args.get('msg')
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=input_text,
-        max_tokens=60,
-        n=1,
-        stop=None,
-        temperature=0.7,
+        engine = "davinci",
+        prompt = user_text,
+        max_tokens = 60,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
     )
 
     # Extract response text from API response
     response_text = response.choices[0].text.strip()
 
-    # Return response as JSON
-    return jsonify({'response': response_text})
+    return str(response_text)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
