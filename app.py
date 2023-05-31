bf28c85996f3160c17c7e758427334959e376db0
PatrykF <103563386+70ziko@users.noreply.github.com>
2023-05-31 06:51:28 +0200

    fixing stuff

--- app.py
+++ app.py
@@ -13,16 +13,28 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+parameters = {
+    "engine": "gpt-3.5-turbo",
+    "max_tokens": 60,
+    "temperature": 0.8,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0,
+    "stop": ["\n", " Human:", " AI:"]
+}
+
 @app.route("/get")
 def get_bot_response():
     user_text = request.args.get('msg')
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
-        # prompt = user_text,
         messages = [{"role": "system", "content": "You are an AI designed to help people fill out surveys and answer questions related to ophthalmology. You specialize in ophthalmology surveys and helping people answer questions about eye problems. You are very good at your job. You are friendly and helpful."},
                     { "role": "user", "content": user_text}],
         max_tokens = 60,
         n = 1,
+        presence_penalty = 0,
+        frequency_penalty = 0,
+        top_p = 1,
         stop = None,
         temperature = 0.8,
     )
@@ -42,6 +54,9 @@ def receive_survey():
 
     # Return a response to the client
     return "Dziękuję za wypełnienie ankiety!"
+
+def receive_messages():
+    pass
 
 
 if __name__ == "__main__":
